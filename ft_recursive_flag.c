5d61e5f0c2ce2057c4083589a9125c44310dae34
Abdennasser Faraji <afaraji@e1r7p4.1337.ma>
2019-08-10 22:35:40 +0100

    normed

--- ft_recursive_flag.c
+++ ft_recursive_flag.c
@@ -12,9 +12,28 @@
 
 #include "ft_ls.h"
 
+int		cmp(char *s1, char *s2)
+{
+	return (ft_strcmp(trim_path(s1), s2));
+}
+
+void	ft_r_helper(t_dlist *head_r, t_dlist *node, char *tab)
+{
+	ft_putchar('\n');
+	ft_putstr(node->path_name);
+	ft_putstr(":\n");
+	if (head_r)
+	{
+		ascii_sort(head_r);
+		sort_by_flag(head_r, tab[3], tab[4]);
+		ft_l_flag(head_r, tab, 1);
+		ft_recursive_flag(head_r, tab);
+		free_list(head_r);
+	}
+}
+
 void	ft_recursive_flag(t_dlist *head, char *tab)
 {
-	t_dlist	*head_r;
 	t_dlist	*node;
 	DIR		*d;
 
@@ -23,8 +42,7 @@ void	ft_recursive_flag(t_dlist *head, char *tab)
 	{
 		if (verify_type(node->path_name) == 1)
 		{
-			if (ft_strcmp(trim_path(node->name), ".") == 0 ||
-			ft_strcmp(trim_path(node->name), "..") == 0)
+			if (cmp((node->name), ".") == 0 || cmp((node->name), "..") == 0)
 			{
 				node = node->next;
 				continue;
@@ -35,21 +53,9 @@ void	ft_recursive_flag(t_dlist *head, char *tab)
 				ft_putstr(node->path_name);
 				ft_putstr(":\n./ft_ls: ");
 				perror(trim_path(node->path_name));
-				node = node->next;
-				continue;
 			}
-			head_r = ft_ls(d, tab[1], node->path_name);
-			ft_putchar('\n');
-			ft_putstr(node->path_name);
-			ft_putstr(":\n");
-			if (head_r)
-			{
-				ascii_sort(head_r);
-				sort_by_flag(head_r, tab[3], tab[4]);
-				ft_l_flag(head_r, tab, 1);
-				ft_recursive_flag(head_r, tab);
-				free_list(head_r);
-			}
+			else
+				ft_r_helper(ft_ls(d, tab[1], node->path_name), node, tab);
 		}
 		node = node->next;
 	}
