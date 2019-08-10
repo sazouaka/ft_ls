10902c2c99ec24d6de8a3687d2cb10bd58b2f533
Abdennasser Faraji <afaraji@e1r7p4.1337.ma>
2019-08-10 19:24:40 +0100

    half norm

--- ft_ls.h
+++ ft_ls.h
@@ -28,50 +28,46 @@
 # define MAX(m,n) (m > n) ? m : n
 # define MONTHS_6 15724800
 
-
-typedef struct  s_dlist
+typedef struct		s_dlist
 {
-    char *name;
-    char *path_name;
-    time_t mtime;
-    struct s_dlist *next;
+	char			*name;
+	char			*path_name;
+	time_t			mtime;
+	struct s_dlist	*next;
 
-}         t_dlist ;
+}					t_dlist;
 
-typedef	struct	s_max
+typedef	struct		s_max
 {
-	int nlink;
-	int uid;
-	int gid;
-	int size;
-	int min;
-	int maj;
-}				t_max;
+	int				nlink;
+	int				uid;
+	int				gid;
+	int				size;
+	int				min;
+	int				maj;
+}					t_max;
 
-t_dlist 	*get_one_file(char *str, char *s2);
-void    	ascii_sort(t_dlist *node);
-void    	ascii_sort_r(t_dlist *node);
-void    	sort_time(t_dlist *node);
-void    	sort_r_time(t_dlist *node);
-void    	sort_by_flag(t_dlist *head, char r, char t);
-int     	flag_search(char c);
-char    	*get_flag_tab(int ac, char **av, int *index);
-void    	print_list(t_dlist *head);
-void    	ft_l_flag(t_dlist *head, char *tab, int d);
-int     	ft_blocks(t_dlist *head);
-t_dlist 	*ft_ls(DIR *dir, char a, char *str);
-
-
-// new functions
-t_dlist 	*get_files(int index, char **argv, char *tab);
-t_dlist 	*get_dirs(int argc, char **argv, char *tab);
-t_dlist 	*get_file(char *str);
-int 		verify_type(char *str);
-void    	display_files(t_dlist *files, char *tab);
-void		permission(char *str);
-int			print_all(t_dlist  *dirs, char *tab, t_dlist  *tmp_file);
-void    	ft_R_flag(t_dlist *head, char *tab);
-char		*trim_path(char *str);
-void		free_list(t_dlist *head);
+t_dlist				*get_one_file(char *str, char *s2);
+void				ascii_sort(t_dlist *node);
+void				ascii_sort_r(t_dlist *node);
+void				sort_time(t_dlist *node);
+void				sort_r_time(t_dlist *node);
+void				sort_by_flag(t_dlist *head, char r, char t);
+int					flag_search(char c);
+char				*get_flag_tab(int ac, char **av, int *index);
+void				print_list(t_dlist *head);
+void				ft_l_flag(t_dlist *head, char *tab, int d);
+int					ft_blocks(t_dlist *head);
+t_dlist				*ft_ls(DIR *dir, char a, char *str);
+t_dlist				*get_files(int index, char **argv, char *tab);
+t_dlist				*get_dirs(int argc, char **argv, char *tab);
+t_dlist				*get_file(char *str);
+int					verify_type(char *str);
+void				display_files(t_dlist *files, char *tab);
+void				permission(char *str);
+int					print_all(t_dlist *dirs, char *tab, t_dlist *tmp_file);
+void				ft_recursive_flag(t_dlist *head, char *tab);
+char				*trim_path(char *str);
+void				free_list(t_dlist *head);
 
 #endif
